297b36ada099f831d0b53d87a114a6e21c9cd6c9
jee599 <jee599@naver.com>
2019-09-17 00:46:06 +0900

    OpenGL
    Wave

---
+++ Wave/Wave/Scene.cpp
@@ -0,0 +1,26 @@
+#include "Scene.h"
+#include "SceneMgr.h"
+
+CScene::CScene()
+{
+	Initialize();
+}
+
+CScene::~CScene()
+{
+	Release();
+}
+
+void CScene::Initialize()
+{
+
+}
+void CScene::Render()
+{
+
+}
+
+void CScene::Release()
+{
+
+}
